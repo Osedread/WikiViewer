853c163aeb3856e561b5f57714b5caec1a3d8fab
Jose Diaz-Gonzalez <osedread@gmail.com>
2017-07-24 15:13:08 +0100

    Fixed animation on page load. Starting to implement the API with pure JS *Hopefully =)*

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,25 @@
 
-var searchIcon = document.getElementById("searchIcon");
-var searchInput = document.getElementById("searchInput");
+var openerBtn = document.getElementById("openerBtn");
+var searchDiv = document.getElementById("searchDiv");
+var container = document.getElementById("container");
+
 
 function opening() {
-    searchIcon.classList.add("zoomIn");
-    console.log("zoomin");
-    searchInput.classList.add("fadeIn");
-    console.log("fadein");
+    openerBtn.classList.add("animateFadeOut");
+    var delayPopIn = setTimeout(function() {
+        if (openerBtn.classList.contains("animateFadeOut")) {
+            container.removeChild(openerBtn);
+            searchDiv.classList.add("fadeIn");
+        }
+    }, 1000);    
 }
 
-searchIcon.addEventListener("click", opening, false);
+
+openerBtn.addEventListener("click", opening, false);
+
+// Create an XMLHttpRequest Object
+var xhr = new XMLHttpRequest(),
+    method = "GET",
+    url = "";
+// Open the request and pass the HTTP Method and the resource
+// request.open("GET", "");
